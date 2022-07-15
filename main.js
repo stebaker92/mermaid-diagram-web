ad81c40ccc8c3f05a107566fdee90e4675d00c3e
Steve Baker <stebaker92@users.noreply.github.com>
2022-07-16 00:10:07 +0100

    Cleanup DOM population

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 window.addEventListener('DOMContentLoaded', () => {
     const params = Object.fromEntries(new URLSearchParams(location.search));
 
-    var diagramEl = document.querySelector(".mermaid-placeholder");
+    var diagramEl = document.querySelector(".mermaid");
     var titleEl = document.querySelector("h2");
-    
+
     if (params.diagram) {
         console.log("Got diagram", decodeURI(params.diagram))
         diagramEl.innerHTML = decodeURI(params.diagram);
         titleEl.innerText = decodeURI(params.title || '');
+    } else {
+        diagramEl.innerHTML = `graph TD
+        A --> B
+        B --> C
+        C`
+        titleEl.innerText = "Sample Diagram";
     }
 
     var config = {
@@ -22,9 +28,4 @@ window.addEventListener('DOMContentLoaded', () => {
     };
 
     mermaid.initialize(config);
-
-    titleEl.style.display = "block";
-
-    diagramEl.className = "mermaid"
-    diagramEl.style.display = "block";
 })
